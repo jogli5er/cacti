6c7aba0f6e5d9a665ca61b6bf480e36fdea0f6ca
Roman Brunner <roman.brunner@siga.swiss>
2024-04-12 17:07:53 +0200

    initial

--- cacti_interface.cc
+++ cacti_interface.cc
@@ -32,7 +32,6 @@
 #include <time.h>
 #include <math.h>
 
-
 #include "area.h"
 #include "basic_circuit.h"
 #include "component.h"
@@ -47,32 +46,41 @@
 
 using namespace std;
 
-
-bool mem_array::lt(const mem_array * m1, const mem_array * m2)
+bool mem_array::lt(const mem_array *m1, const mem_array *m2)
 {
-  if (m1->Nspd < m2->Nspd) return true;
-  else if (m1->Nspd > m2->Nspd) return false;
-  else if (m1->Ndwl < m2->Ndwl) return true;
-  else if (m1->Ndwl > m2->Ndwl) return false;
-  else if (m1->Ndbl < m2->Ndbl) return true;
-  else if (m1->Ndbl > m2->Ndbl) return false;
-  else if (m1->deg_bl_muxing < m2->deg_bl_muxing) return true;
-  else if (m1->deg_bl_muxing > m2->deg_bl_muxing) return false;
-  else if (m1->Ndsam_lev_1 < m2->Ndsam_lev_1) return true;
-  else if (m1->Ndsam_lev_1 > m2->Ndsam_lev_1) return false;
-  else if (m1->Ndsam_lev_2 < m2->Ndsam_lev_2) return true;
-  else return false;
+  if (m1->Nspd < m2->Nspd)
+    return true;
+  else if (m1->Nspd > m2->Nspd)
+    return false;
+  else if (m1->Ndwl < m2->Ndwl)
+    return true;
+  else if (m1->Ndwl > m2->Ndwl)
+    return false;
+  else if (m1->Ndbl < m2->Ndbl)
+    return true;
+  else if (m1->Ndbl > m2->Ndbl)
+    return false;
+  else if (m1->deg_bl_muxing < m2->deg_bl_muxing)
+    return true;
+  else if (m1->deg_bl_muxing > m2->deg_bl_muxing)
+    return false;
+  else if (m1->Ndsam_lev_1 < m2->Ndsam_lev_1)
+    return true;
+  else if (m1->Ndsam_lev_1 > m2->Ndsam_lev_1)
+    return false;
+  else if (m1->Ndsam_lev_2 < m2->Ndsam_lev_2)
+    return true;
+  else
+    return false;
 }
 
-
-
-void uca_org_t::find_delay() 
+void uca_org_t::find_delay()
 {
-  mem_array * data_arr = data_array2;
-  mem_array * tag_arr  = tag_array2;
+  mem_array *data_arr = data_array2;
+  mem_array *tag_arr = tag_array2;
 
   // check whether it is a regular cache or scratch ram
-  if (g_ip->pure_ram|| g_ip->pure_cam || g_ip->fully_assoc)
+  if (g_ip->pure_ram || g_ip->pure_cam || g_ip->fully_assoc)
   {
     access_time = data_arr->access_time;
   }
@@ -94,6 +102,7 @@ void uca_org_t::find_delay()
   // Normal access: tag array access and data array access happen in parallel.
   // But, the data array will wait for the way-select and transfer only the
   // appropriate block over the h-tree.
+  // TODO: This is where we can get the senseamp mux decoder timing
   else
   {
     access_time = MAX(tag_arr->access_time + data_arr->delay_senseamp_mux_decoder,
@@ -102,28 +111,24 @@ void uca_org_t::find_delay()
   }
 }
 
-
-
 void uca_org_t::find_energy()
 {
-  if (!(g_ip->pure_ram|| g_ip->pure_cam || g_ip->fully_assoc))//(g_ip->is_cache)
+  if (!(g_ip->pure_ram || g_ip->pure_cam || g_ip->fully_assoc)) //(g_ip->is_cache)
     power = data_array2->power + tag_array2->power;
   else
     power = data_array2->power;
 }
 
-
-
 void uca_org_t::find_area()
 {
-  if (g_ip->pure_ram|| g_ip->pure_cam || g_ip->fully_assoc)//(g_ip->is_cache == false)
+  if (g_ip->pure_ram || g_ip->pure_cam || g_ip->fully_assoc) //(g_ip->is_cache == false)
   {
-    cache_ht  = data_array2->height;
+    cache_ht = data_array2->height;
     cache_len = data_array2->width;
   }
   else
   {
-    cache_ht  = MAX(tag_array2->height, data_array2->height);
+    cache_ht = MAX(tag_array2->height, data_array2->height);
     cache_len = tag_array2->width + data_array2->width;
   }
   area = cache_ht * cache_len;
@@ -132,14 +137,14 @@ void uca_org_t::find_area()
 void uca_org_t::adjust_area()
 {
   double area_adjust;
-  if (g_ip->pure_ram|| g_ip->pure_cam || g_ip->fully_assoc)
+  if (g_ip->pure_ram || g_ip->pure_cam || g_ip->fully_assoc)
   {
-    if (data_array2->area_efficiency/100.0<0.2)
+    if (data_array2->area_efficiency / 100.0 < 0.2)
     {
-    	//area_adjust = sqrt(area/(area*(data_array2->area_efficiency/100.0)/0.2));
-    	area_adjust = sqrt(0.2/(data_array2->area_efficiency/100.0));
-    	cache_ht  = cache_ht/area_adjust;
-    	cache_len = cache_len/area_adjust;
+      // area_adjust = sqrt(area/(area*(data_array2->area_efficiency/100.0)/0.2));
+      area_adjust = sqrt(0.2 / (data_array2->area_efficiency / 100.0));
+      cache_ht = cache_ht / area_adjust;
+      cache_len = cache_len / area_adjust;
     }
   }
   area = cache_ht * cache_len;
@@ -147,28 +152,27 @@ void uca_org_t::adjust_area()
 
 void uca_org_t::find_cyc()
 {
-  if ((g_ip->pure_ram|| g_ip->pure_cam || g_ip->fully_assoc))//(g_ip->is_cache == false)
+  if ((g_ip->pure_ram || g_ip->pure_cam || g_ip->fully_assoc)) //(g_ip->is_cache == false)
   {
     cycle_time = data_array2->cycle_time;
   }
   else
   {
     cycle_time = MAX(tag_array2->cycle_time,
-                    data_array2->cycle_time);
+                     data_array2->cycle_time);
   }
 }
 
-uca_org_t :: uca_org_t()
-:tag_array2(0),
- data_array2(0)
+uca_org_t ::uca_org_t()
+    : tag_array2(0),
+      data_array2(0)
 {
-
 }
 
-void uca_org_t :: cleanup()
+void uca_org_t ::cleanup()
 {
-	  if (data_array2!=0)
-		  delete data_array2;
-	  if (tag_array2!=0)
-		  delete tag_array2;
+  if (data_array2 != 0)
+    delete data_array2;
+  if (tag_array2 != 0)
+    delete tag_array2;
 }
